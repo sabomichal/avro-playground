858594c847b3357b399341068ca9b8f73a27244c
Marius Barbulescu <marius.barbulescu@gmail.com>
2021-11-21 11:29:23 +0100

    add possible Jackson solution

--- jackson/src/main/java/Main.java
+++ jackson/src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
+        //TODO check https://www.javaallin.com/code/avro-generated-class-issue-with-json-conversion-kotlin.html
     }
 }
